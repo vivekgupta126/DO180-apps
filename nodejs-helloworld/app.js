da75116a31827f63c33c7032ce297eb96a1278ce
vivekgupta126 <62791424+vivekgupta126@users.noreply.github.com>
2020-12-28 14:57:20 +0530

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Welcome to Azure!');
 });
 
 app.listen(8080, function () {
